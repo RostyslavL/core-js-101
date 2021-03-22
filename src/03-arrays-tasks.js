69539f908b6ffc65369ddee03847fcc5b0784eb5
Rostyslav Lohvynov <rostyslav_lohvynov@sevenet.pl>
2021-03-22 08:07:35 +0100

    fix :  ESLint correction

--- src/03-arrays-tasks.js
+++ src/03-arrays-tasks.js
@@ -234,7 +234,7 @@ function toArrayOfSquares(arr) {
  *   [ 0, 0, 0, 0, 0]         => [ 0, 0, 0, 0, 0]
  *   [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 ] => [ 1, 3, 6, 10, 15, 21, 28, 36, 45, 55 ]
  */
-function getMovingSum(/*arr*/) {
+function getMovingSum(/* arr */) {
   throw new Error('Not implemented');
 }
 
